03a1aced562317731de12eca8aa07ee25d51b512
KavinduMihirangaZincat <148061254+KavinduMihirangaZincat@users.noreply.github.com>
2025-03-03 18:39:05 +0530

    add student controller

--- backend/controller/student.controller.js
+++ backend/controller/student.controller.js
@@ -3,6 +3,7 @@ const getStudents = async (req, res) => {
         const student = await Student.find();
         res.status(200).json({ data: student });
 
+        
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
