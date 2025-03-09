f0ee69bc25330a2c58b395bbc22933a0164cd51e
vidushalakshan <vidushalakshan7890@gmail.com>
2025-03-09 20:53:22 +0530

    aded resources controller content

---
+++ backend/controller/resources.controller.js
@@ -0,0 +1,70 @@
+const Resource = require("../models/resources.model");
+
+const getResources = async (req, res) => {
+  try {
+    const resource = await Resource.find();
+    res.status(200).json({ data: resource });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createResource = async (req, res) => {
+  try {
+    console.log(req.body);
+    res.status(201).json({ data: resource });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getResource = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resource = await Resource.findById(id);
+    res.status(200).json({ data: resource });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateResource = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resource = await Resource.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!resource) {
+      return res.status(404).json({ message: "Resource not found" });
+    }
+
+    const updateresource = await Resource.findById(id);
+    res.status(200).json({ data: updateresource });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteResource = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const resource = await Resource.findByIdAndDelete(id);
+
+    if (!resource) {
+      return res.status(404).json({ message: "Resource not found" });
+    }
+    res.status(200).json({ message: "Resource deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getResources,
+  createResource,
+  getResource,
+  updateResource,
+  deleteResource,
+};
