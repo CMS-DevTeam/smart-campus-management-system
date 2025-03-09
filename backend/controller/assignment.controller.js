066e2741caae7035f2526366d55f948792e6e056
vidushalakshan <vidushalakshan7890@gmail.com>
2025-03-09 20:20:32 +0530

    aded assignment controller content

---
+++ backend/controller/assignment.controller.js
@@ -0,0 +1,71 @@
+const Assignment = require("../models/assignment.model");
+
+const getAssignments = async (req, res) => {
+  try {
+    const assignment = await Assignment.find();
+    res.status(200).json({ data: assignment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createAssignment = async (req, res) => {
+  try {
+    console.log(req.body);
+    res.status(201).json({ data: assignment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAssignment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const assignment = await Assignment.findById(id);
+    res.status(200).json({ data: assignment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateAssignment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const assignment = await Assignment.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!assignment) {
+      return res.status(404).json({ message: "Assignment not found" });
+    }
+
+    const updateassignment = await Assignment.findById(id);
+    res.status(200).json({ data: updateassignment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteAssignment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const assignment = await Assignment.findByIdAndDelete(id);
+
+    if (!assignment) {
+      return res.status(404).json({ message: "Assignment not found" });
+    }
+    res.status(200).json({ message: "Assignment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAssignments,
+  createAssignment,
+  getAssignment,
+  updateAssignment,
+  deleteAssignment,
+};
